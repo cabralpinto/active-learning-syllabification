944585720d607ba941063469c6cfac67714ea610
cabralpinto <jmcabralpinto@gmail.com>
2021-12-07 17:15:35 +0000

    renamed folders

---
+++ bilstm-cnn-crf (python)/src/bilstm.py
@@ -0,0 +1,9 @@
+from sklearn.base import BaseEstimator, ClassifierMixin
+from modAL import ActiveLearner
+import sklearn
+
+class BiLSTM(BaseEstimator, ClassifierMixin):
+    pass
+
+if __name__ == '__main__':
+    ActiveLearner
