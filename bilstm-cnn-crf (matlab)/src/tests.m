440640d8a2426d028e793bcf1da6d96cd85f5c7c
cabralpinto <jmcabralpinto@gmail.com>
2021-12-16 13:40:30 +0000

    config file and draft of the final model

--- bilstm-cnn-crf (matlab)/src/tests.m
+++ bilstm-cnn-crf (matlab)/src/tests.m
@@ -1,7 +1,30 @@
-% settings
-dataset = "french";
+%% configuration
+config = jsondecode(fileread("config.json"));
+%% data
 % load dataset
-text = insertAfter(fileread(fullfile("data", dataset + ".txt")), ...
+text = insertAfter(fileread(fullfile("data", config.dataset + ".txt")), ...
     sprintf("\r\n\r\n"), "   ");
-words = split(string(text(1:5:end - 5)));
-targets = cell2mat(pad(split(text(3:5:end - 5)), '0')) - '0';
+% extract inputs and outputs
+inputs = split(string(text(1:5:end - 5)));
+outputs = cell2mat(pad(split(text(3:5:end - 5)), '0')) - '0';
+% generate train/test indices
+[train, ~, test] = dividerand(numel(inputs), 0.7, 0, 0.3);
+%% model
+layers = [
+    sequenceInputLayer(1)
+    wordEmbeddingLayer(config.layers.embedding.dimension, ...
+        numel(inputs(train)))
+    dropoutLayer(config.layers.dropout.probability)
+    bilstmLayer(config.layers.bilstm.units)
+    repmat(convolution2dLayer([1 1], config.layers.cnn.filters), ...
+        [config.layers.cnn.layers 1])
+    maxPooling2dLayer([1 1])
+    concatenationLayer(1, 2)
+    fullyConnectedLayer(2 * size(outputs, 2)) % time distributed?
+    softmaxLayer
+    classificationLayer];
+graph = layerGraph(layers);
+graph = disconnectLayers(graph, "biLSTM", "conv_1");
+graph = connectLayers(graph, "word-embedding", "conv_1");
+graph = connectLayers(graph, "biLSTM", "concat/in2");
+analyzeNetwork(graph);
